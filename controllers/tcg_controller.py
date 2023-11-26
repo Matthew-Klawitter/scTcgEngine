139d2f1496e813191e0617cd256acd73d02c0955
matt <mattklawitt@gmail.com>
2023-11-26 00:20:52 -0500

    Added endpoints for cards and initial backend logic

--- controllers/tcg_controller.py
+++ controllers/tcg_controller.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from models.tcg import CardSets, CardSet
+from models.tcg import CardSets, CardSet, Cards, Card
 
 
 def create_card_set(card_set_model: CardSets, db: Session):
@@ -41,3 +41,43 @@ def update_card_set(id: int, card_set_model: CardSets, db: Session):
         db.commit()
     db.refresh(card_set)
     return card_set
+
+
+def create_card(card_model: Cards, db: Session):
+    card = Card(**card_model.model_dump())
+    db.add(card)
+    db.commit()
+    db.refresh(card)
+    return card
+
+
+def get_all_cards(db: Session):
+    cards = db.query(Card).all()
+    return cards
+
+
+def get_card(id: int, db: Session):
+    card = db.query(Card).get(id)
+    return card
+
+
+def delete_card(id: int, db: Session):
+    card = db.query(Card).get(id)
+    if card is None:
+        return card
+    else:
+        db.delete(card)
+        db.commit()
+    return card
+
+
+def update_card(id: int, card_model: Cards, db: Session):
+    card = db.query(Card).get(id)
+    if card is None:
+        return card
+    else:
+        for var, value in vars(card_model).items():
+            setattr(card, var, value) if value else None
+        db.commit()
+    db.refresh(card)
+    return card
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from starlette.responses import Response
 from starlette.status import HTTP_204_NO_CONTENT
 
 from controllers.tcg_controller import update_card_set, delete_card_set, get_card_set, get_all_card_sets, \
-    create_card_set
+    create_card_set, update_card, delete_card, get_card, get_all_cards, create_card
 from database import get_db
-from models.tcg import CardSets
+from models.tcg import CardSets, Cards
 
 app = FastAPI()
 
@@ -51,3 +51,40 @@ def update(id: int, card_set_model: CardSets, db: Session = Depends(get_db)):
     if card_set is None:
         raise HTTPException(status_code=404, detail=f"Card Set with id {id} does not exist")
     return card_set
+
+
+@app.post("/tcg/card")
+def create(card_model: Cards, db: Session = Depends(get_db)):
+    card = create_card(card_model, db)
+    return card
+
+
+@app.get("/tcg/card")
+def get(db: Session = Depends(get_db)):
+    cards = get_all_cards(db)
+    return cards
+
+
+@app.get("/tcg/card/{id}")
+def get(id: int, db: Session = Depends(get_db)):
+    card = get_card(id, db)
+    if card is None:
+        raise HTTPException(status_code=404, detail=f"Card with id {id} does not exist")
+    else:
+        return card
+
+
+@app.delete("/tcg/card/delete/{id}")
+def delete(id: int, db: Session = Depends(get_db)):
+    card = delete_card(id, db)
+    if card is None:
+        raise HTTPException(status_code=404, detail=f"Card with id {id} does not exist")
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.put("/tcg/card/update/{id}")
+def update(id: int, card_model: Cards, db: Session = Depends(get_db)):
+    card = update_card(id, card_model, db)
+    if card is None:
+        raise HTTPException(status_code=404, detail=f"Card with id {id} does not exist")
+    return card
